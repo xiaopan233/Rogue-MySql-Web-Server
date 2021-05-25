ba326dd354e3d5f8a43bacc5d58ad25a3b9d538d
Xiaopan233 <admin@xiaopan233.club>
2021-05-25 15:22:57 +0800

    update

--- server.py
+++ server.py
@@ -1,36 +1,44 @@
 import socket
-
+import threading
 
 password = "/ebf734024jto485"
+port = 8886
+
+def server(conn, addr):
+	try:
+		file = open('mysql.log', 'rb')
+
+		request = conn.recv(1024)
+		method = request.split(' ')[0]
+		src  = request.split(' ')[1]
+
+		logfile = open("rms_server_log.txt","a")
+		logfile.write('Connect by: ip=' + addr[0] + " port=" + str(addr[1]) + " url=" + src + "\n")
+		logfile.close()
+
+		if method == 'GET' and src == password:
+			content = "HTTP/1.x 200 ok\r\nContent-Type: text/html\r\n\r\n"
+			content += file.read()
+			conn.sendall(content)
+
+	except:
+		print("Not Http Request or other error....")
+	finally:
+		file.close()
+		conn.close()
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('0.0.0.0', 8886))
-sock.listen(100)
+sock.bind(('0.0.0.0', port))
+sock.listen(1)
+
 
 while True:
-# maximum number of requests waiting
 	conn, addr = sock.accept()
-	request = conn.recv(1024)
-	method = request.split(' ')[0]
-	src  = request.split(' ')[1]
-	logfile = open("rms_server_log.txt","a")
-	logfile.write('Connect by: ip=' + addr[0] + " port=" + str(addr[1]) + " url=" + src + "\n")
-	logfile.close()
+	threading.Thread(target=server,args=(conn, addr)).start()
 
-	#deal wiht GET method
-	if method == 'GET':
-		if src == password:
-			content = "HTTP/1.x 200 ok\r\nContent-Type: text/html\r\n\r\n"
-			file = open('mysql.log', 'rb')
-			content += file.read()
-			file.close()
-		else:
-			conn.close()
-			continue
-	else:
-		conn.close()
-		continue
 
-	conn.sendall(content)
-	#close connection
-	conn.close()
+
+	
+
+	
